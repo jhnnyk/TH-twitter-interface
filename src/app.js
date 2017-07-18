684c1beb46c04ce14be015653090aae82d3d78e6
John Knotts <jhnnyk@gmail.com>
2017-07-18 14:28:44 -0600

    basic view template working

--- src/app.js
+++ src/app.js
@@ -2,7 +2,14 @@ const express = require('express')
 
 const app = express()
 
-app.use('/static', express.static('public'))
+app.use('/static', express.static(__dirname + '/public'))
+
+app.set('view engine', 'pug')
+app.set('views', __dirname + '/views')
+
+const mainRoutes = require('./routes')
+
+app.use(mainRoutes)
 
 app.listen(3000, () => {
   console.log('The app is running on localhost:3000!')
