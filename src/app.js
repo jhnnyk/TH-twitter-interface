6e4af7ca898a9257db86108dcfbaeba0c3a0c2d7
John Knotts <jhnnyk@gmail.com>
2017-07-18 14:06:19 -0600

    add static files

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require('express')
 
 const app = express()
 
+app.use('/static', express.static('public'))
+
 app.listen(3000, () => {
   console.log('The app is running on localhost:3000!')
 })
